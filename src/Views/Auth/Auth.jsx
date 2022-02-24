f10427383329874a1b2c41c954b922b7f825f936
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-23 18:54:03 -0700

    Write Auth skeleton

--- src/Views/Auth/Auth.jsx
+++ src/Views/Auth/Auth.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <h2>Welcome back fellow Acmean!</h2>
+    </div>
+  );
 }
