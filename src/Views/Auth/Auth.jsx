4ad8202c8ef4664a6e3223ddcd87ece6017bdc23
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-24 12:58:20 -0700

    Write Auth

--- src/Views/Auth/Auth.jsx
+++ src/Views/Auth/Auth.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import UserForm from '../../Components/UserForm/UserForm';
 import { useUser } from '../../Context/UserContext';
+import { signInUser, signUpUser } from '../../services/users';
 import './Auth.css';
 
 export default function Auth({ isSigningUp = false }) {
   const history = useHistory();
-  const { user } = useUser();
+  const { setUser } = useUser();
+
+  const handleAuth = async (email, password) => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      if (isSigningUp) {
+        // sign user up in supabase
+        await signUpUser(email, password);
+        // redirect to confirm email
+        history.push('/confirm-email');
+      } else {
+        // if not, sign in user
+        const resp = await signInUser(email, password);
+        // set signed in user into context
+        setUser({ id: resp.id, email: resp.email });
+        // redirect to profile
+        history.replace('/profile');
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 
   return (
-    <>
-      if (isSigningUp){' '}
-      {
-        // sign user up in supabase
-        //
-      }
-    </>
+    <div>
+      <h2>{isSigningUp ? 'Sign Up' : 'Sign In'}</h2>
+
+      <UserForm onSubmit={handleAuth} label={isSigningUp ? 'Sign Up' : 'Sign In'} />
+
+      {isSigningUp ? (
+        <p>
+          Have an account? <Link to="/login">Sign In</Link>
+        </p>
+      ) : (
+        <p>
+          Need an account? <Link to="/register">Sign Up</Link>
+        </p>
+      )}
+    </div>
   );
 }
