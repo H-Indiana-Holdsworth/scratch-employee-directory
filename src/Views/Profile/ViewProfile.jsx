8cca66aec1109827a11189f8a105e419771783eb
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-25 13:32:26 -0700

    Add profile edit redirect button

--- src/Views/Profile/ViewProfile.jsx
+++ src/Views/Profile/ViewProfile.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useProfile } from '../../Context/ProfileContext';
 
 export default function ViewProfile() {
   const { profile, loading } = useProfile();
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile/edit');
+  };
 
   const userProfile = (
     <div>
       <p>Name: {profile.name}</p>
       <p>Birthday: {profile.birthday}</p>
       <p>Bio: {profile.bio}</p>
+
+      <button onClick={handleClick}>Edit Profile</button>
     </div>
   );
 
